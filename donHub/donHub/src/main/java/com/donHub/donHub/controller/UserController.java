4c9092c3f673a2c8234829d09fbcf3cb2a33066c
AshishTanneeru-pfw <tanna01@pfw.edu>
2023-10-29 22:26:56 -0400

    Added documentation for user controller file

--- donHub/donHub/src/main/java/com/donHub/donHub/controller/UserController.java
+++ donHub/donHub/src/main/java/com/donHub/donHub/controller/UserController.java
@@ -18,61 +18,103 @@ import org.springframework.web.bind.annotation.RestController;
 import com.donHub.donHub.model.User;
 import com.donHub.donHub.service.UserServiceI;
 
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
-@Autowired
-private UserServiceI userService;
+	@Autowired
+	private UserServiceI userService;
 
-@GetMapping("/getAllUsers")
-public ResponseEntity<Object> getAllUsers() {
-	List<User> users = userService.getAllUsers();
-	return users != null ? ResponseEntity.status(HttpStatus.FOUND).body(users)
-			: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found!");
-}
+	/**
+	 * Retrieves all users from the database.
+	 *
+	 * @return ResponseEntity<Object> containing a list of users if found, otherwise
+	 *         a message indicating no users found
+	 */
 
+	@GetMapping("/getAllUsers")
+	public ResponseEntity<Object> getAllUsers() {
+		List<User> users = userService.getAllUsers();
+		return users != null ? ResponseEntity.status(HttpStatus.FOUND).body(users)
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found!");
+	}
 
-@GetMapping("/getUserById/{userId}")
-public ResponseEntity<Object> getUserById(@PathVariable Long userId) {
-	Optional<User> user = userService.getUserById(userId);
-	return user != null ? ResponseEntity.status(HttpStatus.FOUND).body(user)
-			: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found!");
+	/**
+	 * Retrieves a user by their ID.
+	 *
+	 * @param userId ID of the user to retrieve
+	 * @return ResponseEntity<Object> containing the user if found, otherwise a
+	 *         message indicating no user found
+	 */
 
-}
+	@GetMapping("/getUserById/{userId}")
+	public ResponseEntity<Object> getUserById(@PathVariable Long userId) {
+		Optional<User> user = userService.getUserById(userId);
+		return user != null ? ResponseEntity.status(HttpStatus.FOUND).body(user)
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found!");
 
-/**
- * method to add user data in the database
- * @param data
- * @return
- */
-@PostMapping()
-public ResponseEntity<Object> addUser(@RequestBody User data) {
-	Optional<User> user =  userService.createUser(data);
-	return user != null ? ResponseEntity.status(HttpStatus.FOUND).body(user)
-			: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found!");
+	}
 
-}
+	/**
+	 * Adds user data to the database.
+	 *
+	 * @param data User object containing data to be added
+	 * @return ResponseEntity<Object> containing the created user if successful,
+	 *         otherwise a message indicating no user found
+	 */
 
-@PutMapping("/updateUser/{userId}")
-public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody User updateUser) {
-	Optional<User> updatedUser = userService.updateUser(userId, updateUser);
-	return updatedUser != null ? ResponseEntity.status(HttpStatus.OK).body(updatedUser)
-			: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No player found to update!");
-}
+	@PostMapping()
+	public ResponseEntity<Object> addUser(@RequestBody User data) {
+		Optional<User> user = userService.createUser(data);
+		return user != null ? ResponseEntity.status(HttpStatus.FOUND).body(user)
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found!");
 
-@DeleteMapping("/deleteAllUsers")
-public ResponseEntity<Object> deleteAllUsers() {
-	Boolean areDeleted = userService.deleteAllUsers();
-	return areDeleted ? ResponseEntity.status(HttpStatus.OK).body("All users deleted successfully!")
-			: ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No users found to delete!");
-}
+	}
 
-@DeleteMapping({ "/deleteUserById/{userId}" })
-public ResponseEntity<Object> deleteUserById(@PathVariable Long userId) {
-	Boolean isDeleted = userService.deleteUserById(userId);
-	return isDeleted ? ResponseEntity.status(HttpStatus.OK).body("User deleted successfully!")
-			: ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No user found to delete!");
-}
+	/**
+	 * Updates user information by their ID.
+	 *
+	 * @param userId     ID of the user to update
+	 * @param updateUser User object containing updated information
+	 * @return ResponseEntity<Object> containing the updated user if successful,
+	 *         otherwise a message indicating no user found to update
+	 */
+
+	@PutMapping("/updateUser/{userId}")
+	public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody User updateUser) {
+		Optional<User> updatedUser = userService.updateUser(userId, updateUser);
+		return updatedUser != null ? ResponseEntity.status(HttpStatus.OK).body(updatedUser)
+				: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No player found to update!");
+	}
+
+	/**
+	 * Deletes all users from the database.
+	 *
+	 * @return ResponseEntity<Object> with a success message if users are deleted
+	 *         successfully, otherwise a message indicating no users found for
+	 *         deletion
+	 */
+
+	@DeleteMapping("/deleteAllUsers")
+	public ResponseEntity<Object> deleteAllUsers() {
+		Boolean areDeleted = userService.deleteAllUsers();
+		return areDeleted ? ResponseEntity.status(HttpStatus.OK).body("All users deleted successfully!")
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No users found to delete!");
+	}
+
+	/**
+	 * Deletes a user by their ID.
+	 *
+	 * @param userId ID of the user to delete
+	 * @return ResponseEntity<Object> with a success message if the user is deleted
+	 *         successfully, otherwise a message indicating no user found for
+	 *         deletion
+	 */
+
+	@DeleteMapping({ "/deleteUserById/{userId}" })
+	public ResponseEntity<Object> deleteUserById(@PathVariable Long userId) {
+		Boolean isDeleted = userService.deleteUserById(userId);
+		return isDeleted ? ResponseEntity.status(HttpStatus.OK).body("User deleted successfully!")
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No user found to delete!");
+	}
 
 }
