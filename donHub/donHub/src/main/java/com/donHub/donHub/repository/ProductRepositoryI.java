da52d2ace2a322fca437127cdd7d0505ffb4dedb
keerthikam_pfw <mulak01@pfw.edu>
2023-11-12 23:57:24 -0500

    added cde fxes

--- donHub/donHub/src/main/java/com/donHub/donHub/repository/ProductRepositoryI.java
+++ donHub/donHub/src/main/java/com/donHub/donHub/repository/ProductRepositoryI.java
@@ -1,5 +1,9 @@
 package com.donHub.donHub.repository;
 
-public interface ProductRepositoryI {
+import org.springframework.data.repository.CrudRepository;
+
+import com.donHub.donHub.model.Product;
+
+public interface ProductRepositoryI extends CrudRepository<Product, Long>{
 
 }
--- donHub/donHub/src/main/java/com/donHub/donHub/service/ProductServiceI.java
+++ donHub/donHub/src/main/java/com/donHub/donHub/service/ProductServiceI.java
@@ -1,9 +1,16 @@
 package com.donHub.donHub.service;
 
-import com.donHub.donHub.model.ProductResponse;
+import com.donHub.donHub.model.Product;
 
 public interface ProductServiceI {
 
-	public ProductResponse addProduct();
-
+	public Product addProduct();
+	
+	public Product getProduct();
+	
+	public Product deleteProduct(Long id);
+	
+	public Product updateProduct();
+	
+	public Product getProductById(Long id);
 }
