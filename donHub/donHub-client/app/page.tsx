bf6402c8febbffe4b65661e9ca34b776fa8b3f21
Tanuja <kollt01@pfw.edu>
2023-11-29 13:01:41 -0500

    Modified signup page

--- donHub/donHub-client/app/page.tsx
+++ donHub/donHub-client/app/page.tsx
@@ -9,8 +9,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Home Page | DonHub",
+  description: "This is Home Page for DonHub",
   // other metadata
 };
 
