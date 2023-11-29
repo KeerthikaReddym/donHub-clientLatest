cc3980f1a7fe63961da1c3d5b5e0505c71a2cbff
keerthikam_pfw <mulak01@pfw.edu>
2023-11-29 13:27:30 -0500

    added code fixes for product

--- donHub/donHub/src/main/java/com/donHub/donHub/service/ProductService.java
+++ donHub/donHub/src/main/java/com/donHub/donHub/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.donHub.donHub.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,68 +10,70 @@ import com.donHub.donHub.repository.ProductRepository;
 
 @Service
 public class ProductService implements ProductServiceI {
-	
+
 	@Autowired
-    private ProductRepository productRepository;
+	private ProductRepository productRepository;
 
+	/**
+	 * Adds a new product to the database.
+	 *
+	 * @return The added product.
+	 */
+	@Override
+	public ProductRequest addProduct(ProductRequest productRequest) {
+		// Implement the logic to add a new product
+		return productRepository.save(productRequest);
+	}
 
-    /**
-     * Adds a new product to the database.
-     *
-     * @return The added product.
-     */
-    @Override
-    public ProductRequest addProduct(ProductRequest productRequest) {
-        // Implement the logic to add a new product
-           return productRepository.save(productRequest);
-    }
+	/**
+	 * Retrieves all products from the database.
+	 *
+	 * @return The list of all products.
+	 */
+	@Override
+	public ProductRequest getProducts() {
+		productRepository.findAll();
+		// Implement the logic to return the list of products
+		return null;
+	}
 
-    /**
-     * Retrieves all products from the database.
-     *
-     * @return The list of all products.
-     */
-    @Override
-    public ProductRequest getProducts() {
-        productRepository.findAll();
-        // Implement the logic to return the list of products
-        return null;
-    }
+	/**
+	 * Deletes a product from the database based on its ID.
+	 *
+	 * @param id The ID of the product to delete.
+	 * @return The deleted product.
+	 */
+	@Override
+	public ProductRequest deleteProduct(Integer id) {
+		productRepository.delete(null);
+		// Implement the logic to delete the product
+		return null;
+	}
 
-    /**
-     * Deletes a product from the database based on its ID.
-     *
-     * @param id The ID of the product to delete.
-     * @return The deleted product.
-     */
-    @Override
-    public ProductRequest deleteProduct(Integer id) {
-        productRepository.delete(null);
-        // Implement the logic to delete the product
-        return null;
-    }
+	/**
+	 * Updates a product in the database.
+	 *
+	 * @return The updated product.
+	 *//*
+		 * @Override public ProductRequest updateProduct(ProductRequest productRequest)
+		 * { // productRepository. return null; }
+		 */
 
-    /**
-     * Updates a product in the database.
-     *
-     * @return The updated product.
-     */
-    @Override
-    public ProductRequest updateProduct() {
-        // Implement the logic to update a product
-        return null;
-    }
+	/**
+	 * Retrieves a product from the database based on its ID.
+	 *
+	 * @param id The ID of the product to retrieve.
+	 * @return The retrieved product.
+	 */
+	@Override
+	public Optional<ProductRequest> getProductById(Integer id) {
+		return productRepository.findById(id);
 
-    /**
-     * Retrieves a product from the database based on its ID.
-     *
-     * @param id The ID of the product to retrieve.
-     * @return The retrieved product.
-     */
-    @Override
-    public ProductRequest getProductById(Integer id) {
-        productRepository.findById(id);
-        // Implement the logic to retrieve a product by ID
-        return null;
-    }
+	}
+
+	@Override
+	public ProductRequest getProductByName(String name) {
+		return null;
+		
+	}
 }
