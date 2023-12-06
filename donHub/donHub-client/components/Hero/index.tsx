7d68c6012173d9412e4a7ff997e94cde4afa09c1
Tanuja <kollt01@pfw.edu>
2023-12-06 18:13:54 -0500

    add code fixes

--- donHub/donHub-client/components/Hero/index.tsx
+++ donHub/donHub-client/components/Hero/index.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                 <h1 className="mb-5 text-4xl font-bold leading-tight text-black dark:text-white sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight don-hub-heading">
+                 <h1 className="mb-5 text-4xl font-bold leading-tight text-black hover:text-primary dark:text-white sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight don-hub-heading">
                   Don Hub
                 </h1>
                 <h1 className="mb-5 text-base font-bold leading-tight text-black dark:text-white sm:text-lg md:text-xl tagline">
